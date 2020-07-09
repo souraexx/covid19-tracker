dc35d4e81fa490f0087c44f83a9c7ebe4400a4e4
Souradeep Dutta <sourae.47@gmail.com>
2020-07-10 03:20:20 +0530

    Published

---
+++ src/api/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) => {
+	let changeAbleUrl = url;
+
+	if (country) {
+		changeAbleUrl = `${url}/countries/${country}`;
+	}
+	try {
+		const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeAbleUrl);
+		return {
+			confirmed,
+			recovered,
+			deaths,
+			lastUpdate
+		};
+	} catch (error) {}
+};
+
+export const fetchDailyData = async () => {
+	try {
+		const { data } = await axios.get(`${url}/daily`);
+		const modififedData = data.map((dailyData) => ({
+			confirmed: dailyData.confirmed.total,
+			deaths: dailyData.deaths.total,
+			date: dailyData.reportDate
+		}));
+		return modififedData;
+	} catch (error) {}
+};
+
+export const fetchCountries = async () => {
+	try {
+		const { data: { countries } } = await axios.get(`${url}/countries`);
+
+		return countries.map((country) => country.name);
+	} catch (error) {}
+};
